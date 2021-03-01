ea0f42355e564fd942ce3fdde56b0ea82566e0e2
Masaharu Kato <saa01068@gmail.com>
2021-03-01 13:24:56 +0900

    updates (just for running)

--- src/schema.py
+++ src/schema.py
@@ -2,7 +2,7 @@
     Database schema module
 """
 from collections import defaultdict
-from typing import Any, Dict, List, NewType, Optional, Iterator, Set, Sequence, Tuple
+from typing import Any, Dict, Iterator, List, NewType, Optional, Union, Set, Sequence, Tuple
 import sys
 import re
 import weakref
@@ -89,7 +89,24 @@ class Column(sqe.SQLExprType):
         self.link_columns = weakref.WeakSet(self.table.db.get_column_by_ref(cref) for cref in self.link_column_refs)
         self.frozen = True
 
+    def unique_alias(self) -> str:
+        if self.only_on_db:
+            return self.name
+        return self.table.name + '.' + self.name
+
+    def opt_unique_alias(self) -> Optional[str]:
+        alias = self.unique_alias()
+        if self.name == alias:
+            return None
+        return alias
+
     def __repr__(self):
+        return f'<Column `{self.name}`>'
+
+    def __hash__(self):
+        return hash((hash(self.table), self.name))
+
+    def vrepr(self):
         return repr((self.name, self.data_type, self.link_columns))
 
     def __str__(self) -> str:
@@ -97,7 +114,7 @@ class Column(sqe.SQLExprType):
 
     def __sqlout__(self, swp:sqe.SQLWithParams):
         if not self.only_on_db:
-            swp.append(self.table, sqe.sqlword('.'))
+            swp.append(self.table).append('.', raw=True)
         return swp.append(sqe.sqlobj(self.name))
 
     def dump(self):
@@ -116,22 +133,22 @@ class Table(sqe.SQLExprType):
     name: str                                          # table name
     _coldict : Dict[ColumnName, Column]                # dict of own columns (column name -> column object)
     _keycol  : Column                                  # primary key column
-    parent_tables : Dict[Table, Tuple[Column, Column]] # parent table name -> own column object
-    child_tables  : Dict[Table, Tuple[Column, Column]] # child table name -> child table's column object
+    parent_tables : Dict['Table', Tuple[Column, Column]] # parent table name -> own column object
+    child_tables  : Dict['Table', Tuple[Column, Column]] # child table name -> child table's column object
     only_on_db    : bool                               # The only table name in the database or not
     frozen        : bool                               # Is frozen or not
 
     def __init__(self, name:TableName, columns:Optional[List[Column]]=None):
-        assert all(isinstance(c, Column) for c in self._coldict)
         self.db = None
         self.name = name
-        self._coldict = {column.name: column for column in self._coldict} if columns else {}
+        self._coldict = {column.name: column for column in columns} if columns else {}
+        assert all(isinstance(c, Column) for c in self._coldict.values())
         _keycols = [col for col in self._coldict.values() if col.is_primary]
         if len(_keycols) > 1:
             raise RuntimeError('Multiple primary keys.')
         self._keycol = _keycols[0] if _keycols else None
-        self.parent_tables = defaultdict(lambda: [])
-        self.child_tables  = defaultdict(lambda: [])
+        self.parent_tables = {}
+        self.child_tables  = {}
         self.only_on_db = True
         self.frozen = False
 
@@ -162,21 +179,36 @@ class Table(sqe.SQLExprType):
         # Generate child and parent tables
         for column in self.columns:
             for destcol in column.link_columns:
-                self.child_tables[destcol.table].append((column, destcol))
+
+                if destcol.table in self.child_tables:
+                    raise RuntimeError(f'Multiple child links found between {column.table.name}.{column.name} and {destcol.table.name}.{destcol.name}')
+                self.child_tables[destcol.table] = (column, destcol)
+
+                if self in destcol.table.parent_tables:
+                    raise RuntimeError(f'Multiple parent links found between {destcol.table.name}.{destcol.name} and {column.table.name}.{column.name}')
                 destcol.table.parent_tables[self] = (destcol, column)
 
         self.frozen = True
 
-    def column(self, column_name:ColumnName) -> Column:
-        """ Get column by column name """
-        if column_name not in self._coldict:
-            raise KeyError(f'Undefined column `{column_name}`.')
-        return self._coldict[column_name]
+    def column(self, column:Union[Column, ColumnName]) -> Column:
+        """ Get column by column name / Check if column is valid """
+        if isinstance(column, Column):
+            if id(column.table) != id(self):
+                raise RuntimeError(f'Unknown column `{column}`')
+            return column
 
-    def col(self, column_name:ColumnName) -> Column:
+        if not isinstance(column, str):
+            raise TypeError(f'Invalid type of column (type:{type(column)}).')
+
+        if column not in self._coldict:
+            raise KeyError(f'Undefined column `{column}`.')
+
+        return self._coldict[column]
+
+    def col(self, column_name:Union[Column, ColumnName]) -> Column:
         return self.column(column_name)
 
-    def __getitem__(self, column_name:ColumnName) -> Column:
+    def __getitem__(self, column_name:Union[Column, ColumnName]) -> Column:
         return self.column(column_name)
 
     def get_table_links(self, dest_table:TableName) -> Iterator[Tuple[Column, Column]]:
@@ -189,17 +221,25 @@ class Table(sqe.SQLExprType):
 
     def get_parent_table_links(self) -> Iterator[Tuple['Table', Tuple[Column, Column]]]: # (table object, (left column, right column))
         """ Get parent table(s) recursively with (table object, the table always exists or not) """
-        for table, (lcol, rcol) in self.parent_tables:
+        for table, (lcol, rcol) in self.parent_tables.items():
             yield (table, (lcol, rcol))
+        for table, (lcol, rcol) in self.parent_tables.items():
             yield from table.get_parent_table_links()
 
     def get_child_table_links(self) -> Iterator[Tuple['Table', Tuple[Column, Column]]]: # (table object, (left column, right column))
         """ Get child table(s) recursively with (table object, the table always exists or not) """
-        for table, (lcol, rcol) in self.child_tables:
+        for table, (lcol, rcol) in self.child_tables.items():
             yield (table, (lcol, rcol))
+        for table, (lcol, rcol) in self.child_tables.items():
             yield from table.get_child_table_links()
 
     def __repr__(self):
+        return f'<Table `{self.name}`>'
+
+    def __hash__(self):
+        return hash((hash(self.db), self.name))
+
+    def vrepr(self):
         return repr((self.name, list(self.columns)))
 
     def __str__(self) -> str:
@@ -207,7 +247,7 @@ class Table(sqe.SQLExprType):
 
     def __sqlout__(self, swp:sqe.SQLWithParams):
         if not self.only_on_db:
-            swp.append(self.db, sqe.sqlword('.'))
+            swp.append(self.db).append('.', raw=True)
         return swp.append(sqe.sqlobj(self.name))
 
     def dump(self):
@@ -231,9 +271,10 @@ class Database(sqe.SQLExprType):
     frozen: bool
 
     def __init__(self, name:str, tables:Optional[List[Table]]=None, *, final:bool=False):
-        assert all(isinstance(t, Table) for t in self._tbldict)
         self.name = name
         self._tbldict = {table.name: table for table in tables} if tables else {}
+        self._coldict = {}
+        assert all(isinstance(t, Table) for t in self._tbldict.values())
         self.frozen = False
         if final:
             self.finalize()
@@ -260,7 +301,7 @@ class Database(sqe.SQLExprType):
 
             # Search columns in the tables
             for column in table.columns:
-                if not column in self._coldict:
+                if not column.name in self._coldict:
                     self._coldict[column.name] = []
                 self._coldict[column.name].append(column)
         
@@ -271,25 +312,52 @@ class Database(sqe.SQLExprType):
         self.frozen = True
                 
                 
-    def table(self, table_name:TableName) -> Table:
-        """ Get table object by table name """
-        if table_name not in self._tbldict:
-            raise KeyError(f'Undefined table `{table_name}`.')
-        return self._tbldict[table_name]
+    def table(self, table:Union[Table, TableName]) -> Table:
+        """ Get table object by table name / Check if table is valid """
+        if isinstance(table, Table):
+            if id(table.db) != id(self):
+                raise RuntimeError(f'Unknown table `{table}`.')
+            return table
+
+        if not isinstance(table, str):
+            raise TypeError(f'Invalid type of table (type:{type(table)}).')
+
+        table_s = table.split('.')
+        if len(table_s) >= 2: # Specified like 'database.table'
+            table = table_s[-1]
+
+        if table not in self._tbldict:
+            raise KeyError(f'Undefined table `{table}`.')
+
+        return self._tbldict[table]
     
-    def __getitem__(self, table_name:TableName) -> Table:
+    def __getitem__(self, table_name:Union[Table, TableName]) -> Table:
         return self.table(table_name)
 
-    def column(self, column_name:ColumnName) -> Column:
-        """ Get column object by column name (Assume just one column) """
-        if column_name not in self._coldict:
-            raise KeyError(f'Undefined column `{column_name}`.')
-        cols = self._coldict[column_name]
+    def column(self, column:Union[Column, ColumnName]) -> Column:
+        """ Get column object by column name (Assume just one column) / Check if column is valid """
+        if isinstance(column, Column):
+            if id(column.table.db) != id(self):
+                raise RuntimeError(f'Unknown column `{column}`.')
+            return column
+
+        if not isinstance(column, str):
+            raise TypeError(f'Invalid type of column. (type:{type(column)})')
+
+        col_s = column.split('.')
+        if len(col_s) >= 2: # Specified like 'database.table.column' or 'table.column'
+            return self.table(col_s[-2]).column(col_s[-1])
+
+        if column not in self._coldict:
+            raise KeyError(f'Undefined column `{column}`.')
+
+        cols = self._coldict[column]
         if len(cols) > 1:
-            raise RuntimeError(f'Multiple columns found. (`{column_name}`)')
+            raise RuntimeError(f'Multiple columns found. (`{column}`)')
+
         return cols[0]
 
-    def col(self, column_name:ColumnName) -> Column:
+    def col(self, column_name:Union[Column, ColumnName]) -> Column:
         return self.column(column_name)
 
     def get_column_by_ref(self, column_ref:ColumnRef) -> Column:
@@ -308,8 +376,14 @@ class Database(sqe.SQLExprType):
             yield from ((rcol, lcol) for lcol, rcol in self[base_table].link_tables[dest_table])
         if base_table in self[dest_table].link_tables:
             yield from self[dest_table].link_tables[base_table]
-
+ 
     def __repr__(self):
+        return f'<Database `{self.name}`>'
+
+    def __hash__(self):
+        return hash(self.name)
+
+    def vrepr(self):
         return repr((self.name, list(self.tables)))
 
     def __str__(self) -> str:
--- src/sqlexpr.py
+++ src/sqlexpr.py
@@ -1,8 +1,9 @@
-from . import expression
-from . import keywords
+""" SQL Expressions module """
 from abc import abstractmethod
 import re
 from typing import Any, Callable, List, Sequence, Optional
+from . import expression
+from . import keywords
 
 _IS_DEBUG = True
 
@@ -12,101 +13,167 @@ class SQLWithParams:
     """ SQL with parameters (using place-holders) """
     _sql_words: List[str]
     _params: Sequence
+    _cached_sql: Optional[str]
+    _cached_sql_minimized: Optional[bool]
 
     def __init__(self, sql=None, params=None):
-        self._sql_words = '' if sql is None else sql
+        self._sql_words = [] if sql is None else sql
         self._params = [] if params is None else params
+        self._cached_sql = None
+        self._cached_sql_minimized = None
 
     def append(self, val, *vals, raw:bool=False):
-        if raw:
-            self._sql_words.append(str(val))
-        elif callable(val):
-            val(self)
-        elif isinstance(val, SQLExprType):
-            val.__sqlout__(self)
-        else:
-            self._sql_words.append(_HOLDER_STR_)
-            self._params.append(val)
+        """ Append new expression(s) """
+        
+        if raw: # Treat as a raw SQL word
+            assert(isinstance(val, str))
+            self._append_raw(str(val))
+
+        elif callable(val): 
+            val(self) # `val` expects to append some expressions to self
+        
+        elif isinstance(val, (tuple, list)): # Join elements with ','
+            if val:
+                self.append(val[0])
+                for v in val[1:]:
+                    self.append(',', raw=True)
+                    self.append(v)
+
+        elif isinstance(val, SQLExprType): # SQLExprType instance has a special method `__sqlout__`
+            val.__sqlout__(self) # `__sqlout__` expects to append some expressions to self
+        
+        else: # Treat as a value
+            self._append_raw(_HOLDER_STR_) # Add placeholder string ('%s')
+            self._params.append(val) # Add an actual value
 
         if vals:
-            return self.append(*vals, raw=raw)
+            return self.append(*vals, raw=raw) # Process the rest expression(s)
+
         return self
 
+    def append_clause(self, clause_name:str, *vals, end:str='\n'):
+        if not vals:
+            return self
+        if vals[0] is None or (isinstance(vals[0], (tuple, list)) and not vals[0]):
+            # print('vals[0]=', vals[0])
+            return self
+
+        self.assert_keyword(clause_name)
+        self.append(clause_name, raw=True)
+        self.append(*vals)
+        if end in (' ', '\n'):
+            return self.append(end, raw=True)
+        return self
+
+    def _append_raw(self, word:str):
+        """ Append a word to the SQL directly """
+        self._sql_words.append(word)
+        self._cached_sql = None
+
     @property
-    def sql(self, minimize:bool=False) -> str:
+    def sql(self, minimize:bool=True) -> str:
+        """ Get current SQL text """
+        # Returns cache if available
+        if self._cached_sql is not None and minimize == self._cached_sql_minimized:
+            return self._cached_sql
+
+        self._cached_sql_minimized = minimize
+
+        # Non-minimized mode (Simply join words using a space)
         if not minimize:
-            return ' '.join(self._sql_words)
+            self._cached_sql = ' '.join(self._sql_words)
 
-        sql = ''
-        pword:str = '' # None 
-        for word in self._sql_words:
-            if pword and word and pword[-1].isalpha() and word[0].isalpha():
-                sql += ' '
-            sql += word
-            pword = word
+        # Minimized mode (Use a space only when needed)
+        else:
+            sql = ''
+            pword:str = '' # None 
+            for word in self._sql_words:
+                if pword and word:
+                    if pword[-1] == ',' or ((self._is_word_char(pword[-1]) or pword[-1] == ')') and (self._is_word_char(word[0]) or word[0] == '(')):
+                        sql += ' '
+                sql += word
+                pword = word
+            self._cached_sql = sql
 
-        return sql
+        return self._cached_sql
 
 
     @property
     def params(self) -> Sequence:
+        """ Get current list (sequence) of parameters """
         return self._params
 
 
     def execute_on(self, func:Callable[[str, Sequence], Any]):
+        """ Execute specified function with current SQL and list of parameters """
         return func(self.sql, self.params)
 
+
+    @staticmethod
+    def assert_keyword(word:str) -> None:
+        if not re.match(r'\w+', word):
+            raise RuntimeError('Invalid SQL word: `{}`'.format(word))
+
+    @staticmethod
+    def _is_word_char(c:str) -> bool:
+        return c.isalnum() or c in ('`', '+', '-', '*', '/', '%', '=', '<', '>', '&', '|', '^', '!', '~', '@', "'", '"')
+
+
+    def __repr__(self):
+        return repr((self.sql, self.params))
+
+    def __iter__(self):
+        return iter([self.sql, self.params])
 
 
 SQLWithParamsMaker = Callable[[SQLWithParams], SQLWithParams]
 
 
 
-class SQLExprType(expression.ExprABC):
+class SQLExprType(expression.ExprType):
     """ Expression class for SQL """
 
     def uop(self, op):
+        """ Returns a unary expression object """
         return UnaryExpr(op, self)
 
     def bop(self, op, y):
+        """ Returns a binary expression object """
         return BinaryExpr(op, self, y)
 
     def rbop(self, op, y):
+        """ Returns a reversed binary expression object """
         return BinaryExpr(op, y, self)
 
     def infunc(self, name, *args):
+        """ Returns a function expression object using self as a first parameter """
         return FuncExpr(name, self, *args)
 
     @abstractmethod
     def __sqlout__(self, swp:SQLWithParams) -> None:
+        """ (Abstract) Output to SQL """
         pass
 
 
-class UnaryExpr(expression.UnaryExpr):
+class UnaryExpr(SQLExprType, expression.UnaryExpr):
     """ UnaryExpr class for SQL """
 
     def __sqlout__(self, swp:SQLWithParams) -> None:
         swp.append(_sql_op(self.op), self.x)
 
 
-class BinaryExpr(expression.BinaryExpr):
+class BinaryExpr(SQLExprType, expression.BinaryExpr):
     """ UnaryExpr class for SQL """
 
     def __sqlout__(self, swp:SQLWithParams) -> None:
         swp.append(in_bracket(self.x, _sql_op(self.op), self.y))
 
 
-class FuncExpr(expression.FuncExpr):
+class FuncExpr(SQLExprType, expression.FuncExpr):
     """ UnaryExpr class for SQL """
 
     def __sqlout__(self, swp:SQLWithParams) -> None:
         swp.append(_sql_func(self.name), in_bracket(self.args))
-
-
-def sqlword(word:str) -> SQLWithParamsMaker:
-    if not re.match(r'\w+', word):
-        raise RuntimeError('Invalid SQL word: `{}`'.format(word))
-    return lambda swp: swp.append(word, raw=True)
 
 
 def sqlobj(name) -> SQLWithParamsMaker:
@@ -119,40 +186,40 @@ def sqlobj(name) -> SQLWithParamsMaker:
 def in_bracket(*vals) -> SQLWithParamsMaker:
     return lambda swp: swp.append('(', raw=True).append(*vals).append(')', raw=True)
 
-def _joined_maker(swp:SQLWithParamsMaker, *vals, sep:str) -> SQLWithParamsMaker:
-    is_first = True
-    for val in vals:
-        if not is_first:
-            swp.append(sep, raw=True)
-        is_first = False
-        swp.append(val)
-    return swp
+# def _joined_maker(swp:SQLWithParamsMaker, *vals, sep:str) -> SQLWithParamsMaker:
+#     is_first = True
+#     for val in vals:
+#         if not is_first:
+#             swp.append(sep, raw=True)
+#         is_first = False
+#         swp.append(val)
+#     return swp
 
-def joined(*vals, sep:str=',') -> SQLWithParamsMaker:
-    return lambda swp: _joined_maker(swp, *vals, sep=sep)
+# def joined(*vals, sep:str=',') -> SQLWithParamsMaker:
+#     return lambda swp: _joined_maker(swp, *vals, sep=sep)
 
-def opt_joined(*vals, sep:str=',') -> Optional[SQLWithParamsMaker]:
-    if not vals:
-        return None
-    return joined(*vals, sep=sep)
+# def opt_joined(*vals, sep:str=',') -> Optional[SQLWithParamsMaker]:
+#     if not vals:
+#         return None
+#     return joined(*vals, sep=sep)
 
-def _chain_maker(swp:SQLWithParamsMaker, *vals) -> SQLWithParamsMaker:
-    for val in vals:
-        swp.append(val)
-    return swp
+# def _chain_maker(swp:SQLWithParamsMaker, *vals) -> SQLWithParamsMaker:
+#     for val in vals:
+#         swp.append(val)
+#     return swp
 
-def chain(*vals) -> SQLWithParamsMaker:
-    return lambda swp: _chain_maker(swp, *vals)
+# def chain(*vals) -> SQLWithParamsMaker:
+#     return lambda swp: _chain_maker(swp, *vals)
 
-def clause(keyword:str, *vals) -> SQLWithParamsMaker:
-    """ keyword clause """
-    return lambda swp: swp.append(sqlword(keyword)).append(*vals)
+# def clause(keyword:str, *vals) -> SQLWithParamsMaker:
+#     """ keyword clause """
+#     return lambda swp: swp.append(sqlword(keyword)).append(*vals)
 
-def opt_clause(keyword:str, val) -> SQLWithParamsMaker:
-    """ optional keyword clause """
-    if val:
-        return lambda swp: swp.append(sqlword(keyword)).append(val)
-    return lambda swp: swp
+# def opt_clause(keyword:str, val) -> SQLWithParamsMaker:
+#     """ optional keyword clause """
+#     if val:
+#         return lambda swp: swp.append(sqlword(keyword)).append(val)
+#     return lambda swp: swp
 
 def ordered_column(colexpr) -> SQLWithParamsMaker:
     """ column with DESC or ASC """
