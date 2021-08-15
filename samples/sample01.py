8bf8b894be00f44c06826ed211bccd115df98f71
Masaharu Kato <saa01068@gmail.com>
2021-08-15 16:25:09 +0900

    add sample (WIP)

---
+++ samples/sample01.py
@@ -0,0 +1,39 @@
+"""
+    Sample program 01
+"""
+
+from datetime import date as Date
+from typing import List
+
+class User:
+    name: str
+    birthday: Date
+
+class SampleDB:
+    users: List[User]
+
+
+def main():
+    db = SampleDB()
+
+    while True:
+        cinput = input()
+        cmdname, *cmdargs = cinput.split()
+
+        if cmdname == 'exit':
+            break
+
+        elif cmdname == 'get':
+            for user in db.users:
+                print(user.name, user.birthday)
+
+        elif cmdname == 'add':
+            db.users.append(User(name=cmdargs[0], birthday=Date.fromisoformat(cmdargs[1])))
+            
+        else:
+            print('Unknown command: %s' % cmdname)
+
+
+if __name__ == '__main__':
+    main()
+
