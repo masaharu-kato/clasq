65e9531bf8d1736237c92bf23f53c0692cadee37
Masaharu Kato <fudai.mk@gmail.com>
2021-12-02 14:58:39 +0900

    support connection pool

--- libsql/connector.py
+++ libsql/connector.py
@@ -3,9 +3,13 @@
 """
 from abc import abstractmethod, abstractproperty
 from typing import List, Optional, Sequence, Union
+import warnings
+
 import mysql.connector # type: ignore
 import mysql.connector.errors # type: ignore
-import mysql.connector.abstracts # type: ignore
+import mysql.connector.abstracts
+from mysql.connector.pooling import MySQLConnectionPool # type: ignore
+
 from .schema import Database as DBSchema
 
 
@@ -66,41 +70,83 @@ class CursorABC:
 class MySQLCursorABC(mysql.connector.abstracts.MySQLCursorAbstract):
     """ MySQLCursorABC """
 
-class MySQLConnection(ConnectionABC):
-    """ MySQL Connection Class """
+class MySQLConnectionABC(ConnectionABC):
 
-    def __init__(self, *args, dictionary:bool=False, named_tuple:bool=False, db_schema:DBSchema, **kwargs):
+    def __init__(self, *args, db_schema:DBSchema, dictionary:bool=False, named_tuple:bool=False, **kwargs):
         super().__init__(db_schema=db_schema)
-        self.cnx         = mysql.connector.connect(*args, **kwargs) # MySQL connection 
+        self.cnx         = self.new_cnx(*args, **kwargs)
         self.cursor_dict = dictionary
         self.cursor_ntpl = named_tuple
 
+    @abstractmethod
+    def new_cnx(self, *args, **kwargs):
+        """ Create a new connection """
+        raise NotImplementedError()
+
     def commit(self):
         """ Commit the current transaction """
+        assert self.cnx is not None
         return self.cnx.commit()
 
-    def create_cursor(self, *args, **kwargs) -> 'MySQLCursor':
+    def create_cursor(self, *args, **options) -> 'MySQLCursor':
         """ Get cursor object """
-        if self.cursor_dict:
-            kwargs['dictionary'] = True
-        if self.cursor_ntpl:
-            kwargs['named_tuple'] = True
+        return MySQLCursor(self, self.cnx.cursor(*args, **self._cursor_options(**options)))
 
-        return MySQLCursor(self, self.cnx.cursor(*args, **kwargs))
+    def _cursor_options(self, **options) -> dict:
+        if self.cursor_dict:
+            options['dictionary'] = True
+        if self.cursor_ntpl:
+            options['named_tuple'] = True
+        return options
 
     def close(self) -> None:
         """ Close cursor """
         if self.cnx is not None:
-            self.cnx.close()
+            try:
+                self.cnx.close()
+            except mysql.connector.errors.OperationalError as e:
+                warnings.warn(str(e))
             self.cnx = None
 
     def __exit__(self, ex_type, ex_value, trace):
         self.close()
 
+class MySQLConnection(MySQLConnectionABC):
+    """ MySQL Connection Class """
+
+    def new_cnx(self, *args, **kwargs):
+        return mysql.connector.connect(*args, **kwargs) # MySQL connection 
+
+class MySQLPooledConnection(MySQLConnectionABC):
+    """ MySQL Connection from connection-pool """
+
+    def __init__(self, *args,
+        db_schema: DBSchema, dictionary: bool = False, named_tuple: bool = False,
+        pool_size:int = 16,
+        **kwargs):
+        print('MySQLPooledConnection: Init with pool_size=%d' % pool_size)
+        self.cnx_pool = MySQLConnectionPool(pool_size=pool_size, *args, **kwargs)
+        super().__init__(*args, db_schema=db_schema, dictionary=dictionary, named_tuple=named_tuple, **kwargs)
+
+    def new_cnx(self, *args, **kwargs):
+        return self.cnx_pool.get_connection()
+
+    def create_cursor(self, *args, **options) -> 'MySQLCursor':
+        self.close()
+        self.cnx = self.new_cnx()
+        return MySQLCursor(self, self.cnx.cursor(*args, **self._cursor_options(**options)))
+
+    # def close(self) -> None:
+    #     """ Close cursor """
+        # if self.cnx is not None:
+        #     self.cnx.close()
+        #     self.cnx = None
+
 
 class MySQLCursor(CursorABC):
     """ Basic MySQL Cursor (with parent `MySQLConnection` instance) """
-    def __init__(self, con:MySQLConnection, cursor:MySQLCursorABC):
+    def __init__(self, con:MySQLConnectionABC, cursor:MySQLCursorABC):
+        print('[Debug] MySQLCursor: Init. ')
         self._con = con
         self.cursor = cursor
 
