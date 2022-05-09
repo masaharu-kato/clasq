79a31c4b396aeb7f397b6f04718e99b16bbe0fdd
Masaharu Kato <fudai.mk@gmail.com>
2022-05-10 01:18:28 +0900

    fix imports

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -2,7 +2,9 @@
     Test libsql.schema
 """
 import pytest
-from libsql.schema.database import Database, Table, Column
+from libsql.schema.database import Database
+from libsql.schema.table import Table
+from libsql.schema.column import Column
 from libsql.syntax.sqltypes import Int, VarChar
 from libsql.syntax.exprs import Object
 from libsql.syntax.query_data import QueryData
