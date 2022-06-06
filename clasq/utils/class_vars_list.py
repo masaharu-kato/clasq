ac407dc067b245e8dbbf3128dce1da62e46696d3
Masaharu Kato <fudai.mk@gmail.com>
2022-06-07 08:43:31 +0900

    add class vars list

---
+++ clasq/utils/class_vars_list.py
@@ -0,0 +1,13 @@
+"""
+    List based on class variables
+"""
+
+from typing import Generic, TypeVar
+
+T = TypeVar('T')
+class ClassVarsList(Generic[T]):
+    """ Listed with class variables """
+    
+    @classmethod
+    def _get(cls, key) -> T:
+        return getattr(cls, key)
