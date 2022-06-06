ffa668f6ac5cb7ba3711d0612963d4693405c58a
Masaharu Kato <fudai.mk@gmail.com>
2022-06-07 08:44:40 +0900

    update sampledb definition syntax

--- tests/sampledb/sample_db.py
+++ tests/sampledb/sample_db.py
@@ -1,13 +1,12 @@
 """
     Sample DB definition
 """
-
-from datetime import datetime
 from typing import Literal as Lt
 
 from clasq.data.database import DatabaseClass
-from clasq.data.table_record import TableClass, ColumnDef
-from clasq.schema.sqltypes import VarChar
+from clasq.data.table_record import TableClass
+from clasq.schema.column import TableColumn
+from clasq.syntax.data_types import DateTime, Int, Nullable, VarChar
 
 
 class SampleDB(DatabaseClass):
@@ -16,23 +15,23 @@ class SampleDB(DatabaseClass):
 
 
 class Category(TableClass, db=SampleDB, name='categories'):
-    name: ColumnDef[VarChar[Lt[64]]]
+    name: TableColumn[VarChar[Lt[64]]]
 
 class Product(TableClass, db=SampleDB, name='products'):
     category: Category
-    name: ColumnDef[VarChar[Lt[128]]]
-    price: ColumnDef[int]
+    name: TableColumn[VarChar[Lt[128]]]
+    price: TableColumn[Int]
 
 class User(TableClass, db=SampleDB, name='users'):
-    name: ColumnDef[VarChar[Lt[64]]]
-    registered_at: ColumnDef[datetime] = None  # type: ignore
+    name: TableColumn[VarChar[Lt[64]]]
+    registered_at: TableColumn[Nullable[DateTime]]
 
 class UserSale(TableClass, db=SampleDB, name='user_sales'):
     user: User
-    sale_at: ColumnDef[datetime] = None  # type: ignore
+    sale_at: TableColumn[Nullable[DateTime]]
 
 class UserSaleProduct(TableClass, db=SampleDB, name='user_sale_products'):
     user_sale: UserSale
     product: Product
-    price: ColumnDef[int]
-    count: ColumnDef[int]
+    price: TableColumn[Int]
+    count: TableColumn[Int]
