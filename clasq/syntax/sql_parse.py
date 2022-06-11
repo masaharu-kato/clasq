126f86323d0f68243638e6b478b1e0e144a44872
Masaharu Kato <saa01068@gmail.com>
2022-06-11 14:00:53 +0900

    update sql type parsing

--- clasq/syntax/sql_parse.py
+++ clasq/syntax/sql_parse.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 import re
 import datetime
 import typing
-from .abc.data_types import DataTypeABC
+from .abc.data_types import AnyDataType, DataTypeABC
 from . import data_types as dt
 
 DATA_TYPE_FROM_SQL_BASE_NAME: dict[bytes, type[DataTypeABC]] = {}
@@ -14,12 +14,14 @@ for cls in dt.ALL_TYPES:
     if name not in DATA_TYPE_FROM_SQL_BASE_NAME:
         DATA_TYPE_FROM_SQL_BASE_NAME[name] = cls
 
-__RE_SQL_TYPE = re.compile(r'(?P<base>\w+)\s*(\((?P<args>[\w\s,]*)\))?')
+__RE_SQL_TYPE = re.compile(r'(?P<base>\w+)\s*(\((?P<args>[\w\s,]*)\))?\s*(?P<base_options>\w+)?')
 __RE_SQL_INT = re.compile(r'-?\d+')
 
 def data_type_from_sql(sql: str) -> type[DataTypeABC]:
     if m := __RE_SQL_TYPE.fullmatch(sql):
-        base_name = m['base'].encode('ascii').upper()
+        base_name: bytes = m['base'].encode('ascii').upper()
+        if base_options := m['base_options']:
+            base_name += b' ' + base_options.encode('ascii').upper()
         try:
             base_type = DATA_TYPE_FROM_SQL_BASE_NAME[base_name]  # type: ignore
         except KeyError as e:
@@ -32,7 +34,8 @@ def data_type_from_sql(sql: str) -> type[DataTypeABC]:
             except TypeError:  # Not a generic type, invalid arguments etc.
                 pass
         return base_type  
-    raise ValueError('Invalid syntax of SQL.', sql)
+    # raise ValueError('Invalid syntax of SQL.', sql)
+    return AnyDataType
 
 
 def __parse_sql_arg(text: str) -> int | str:
